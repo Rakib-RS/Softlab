93e56ccce38258500583a5dda4e656d05dd5096a
Golam Rabbi <hridoyrabbi97@gmail.com>
2019-04-22 18:17:09 +0600

    Student information

---
+++ Department_Info-Spring-Boot--master/src/main/java/com/simple/apidesign/department_info/StudentsController.java
@@ -0,0 +1,62 @@
+package com.simple.apidesign.department_info;
+
+import java.util.List;
+import com.simple.apidesign.department_info.model.Students;
+import com.simple.apidesign.department_info.repositories.StudentsRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+
+
+@RestController
+@RequestMapping("/instructor")
+
+public class StudentsController {
+	
+  @Autowired
+  private StudentsRepository repository;
+  
+  //Get
+  
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public List<Students> getAllStudents() {
+    return repository.findAll();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Students getStudentsById(@PathVariable("id") ObjectId id) {
+    return repository.findBy_id(id);
+  }
+  
+  //Put
+  
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public Students modifyStudentById(@PathVariable("id") ObjectId id, @Valid @RequestBody Students students) {
+	  students.set_id(id);
+      repository.save(students);
+      return repository.findBy_id(id);
+  }
+  
+  //post
+  
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  public Students createStudents(@Valid @RequestBody Students students) {
+	  students.set_id(ObjectId.get());
+	  repository.save(students);
+	  return students;
+  }
+  
+  //delete
+  
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public void deleteStudents(@PathVariable ObjectId id) {
+    repository.delete(repository.findBy_id(id));
+  }
+  
+}
---
+++ Department_Info-Spring-Boot--master/src/main/java/com/simple/apidesign/department_info/repositories/StudentsRepository.java
@@ -0,0 +1,12 @@
+package com.simple.apidesign.department_info.repositories;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.simple.apidesign.department_info.model.Students;
+
+
+public interface StudentsRepository extends MongoRepository<Students, String> {
+	
+  Students findBy_id(ObjectId _id);
+}
